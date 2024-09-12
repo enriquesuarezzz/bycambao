af11b56b0daa8582f5b0d10160d7ca7706044ac3
enriquesuarezzz <enriquesuarezmartin@gmail.com>
2024-09-12 16:50:32 +0100

    basicSetUp

---
+++ tailwind.config.ts
@@ -0,0 +1,23 @@
+import type { Config } from 'tailwindcss'
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        background: 'var(--background)',
+        foreground: 'var(--foreground)',
+        orange: '#bb5129',
+      },
+      maxWidth: {
+        '8xl': '1400px',
+      },
+    },
+  },
+  plugins: [],
+}
+export default config
